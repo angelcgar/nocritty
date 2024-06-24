84997fa84b2ad4d0908753362dacb994aae530de
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-06-24 06:44:44 -0600

    añadir caracteristica tamaño de fuente y opacidad

--- index.js
+++ index.js
@@ -1,15 +1,85 @@
-const fs = require('node:fs')
-const os = require('node:os');
-const path = require('node:path');
+import { readFile, writeFile } from "node:fs";
+import { homedir } from "node:os";
+import { join } from "node:path";
 
-const fileFirst = './test.txt'
-const fileSecong = path.join(os.homedir(), '.config/alacritty/alacritty.toml');
+import inquirer from "inquirer";
 
-fs.readFile(fileSecong, 'utf8', (err, data)=> {
-    if (err) {
-    console.error('mi error',err);
-    return;
+const configPath = join(homedir(), ".config/alacritty/alacritty.toml");
+
+function readConfig() {
+  return new Promise((resolve, reject) => {
+    readFile(configPath, "utf8", (err, data) => {
+      if (err) {
+        // console.error("mi error", err);
+        reject(err);
+        return;
+      }
+      // console.log(data);
+      resolve(data);
+    });
+  });
+}
+
+function writeConfig(data) {
+  return new Promise((resolve, reject) => {
+    writeFile(configPath, data, "utf-8", (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve();
+    });
+  });
+}
+
+async function modifyConfig() {
+  try {
+    const config = await readConfig();
+    console.log("Cofiguracion actual");
+    console.log(config);
+
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "font_size",
+        message: "Nuevo tamaño de fuente:",
+      },
+      {
+        type: "input",
+        name: "opacity",
+        message: "Nueva opacidad:",
+      },
+      // todo: Agregar mas preguntas
+    ]);
+
+    // const newConfig = config.replace("size =", `size = ${answers.font_size}`);
+    // const newConfig = config.replace(
+    //   /size\s*=\s*\d+/,
+    //   `size = ${answers.font_size}`
+    // );
+
+    // newConfig.replace(/opacity\s*=\d+/, `opacity = ${answers.opacity}`);
+    let newConfig = config.replace(
+      /size\s*=\s*\d+/,
+      `size = ${answers.font_size}`
+    );
+
+    newConfig = newConfig.replace(
+      /opacity\s*=\s*\d+(\.\d+)?/,
+      `opacity = ${answers.opacity}`
+    );
+
+    console.log(answers.font_size);
+    console.log(answers.opacity);
+
+    await writeConfig(newConfig);
+    // console.log(config);
+
+    console.log("Configuración actualizada con éxito.");
+  } catch (err) {
+    console.error("Error: ", err);
   }
-  console.log(data);
-})
+}
 
+modifyConfig();
